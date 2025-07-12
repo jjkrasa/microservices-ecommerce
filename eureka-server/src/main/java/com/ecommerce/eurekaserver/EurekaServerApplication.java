7797ff05dfa8f4b8e4ff788bb28b0f80f29e7ffa
Jakub Josef Krása <jjkrasa@gmail.com>
2025-07-12 17:06:58 +0200

    package naming updated for eureka-server

--- eureka-server/src/main/java/com/ecommerce/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ecommerce/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.discovery.eureka_server;
+package com.ecommerce.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/ecommerce/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/ecommerce/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ecommerce.discovery.eureka_server;
+package com.ecommerce.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
